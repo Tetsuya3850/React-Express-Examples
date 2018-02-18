7d984a2697c7bf7af39102298a113bf2f23441a7
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-02-18 10:54:57 +0900

    added icons to ui

--- quiz/client/src/QuizCard.js
+++ quiz/client/src/QuizCard.js
@@ -2,12 +2,43 @@ import React, { Component } from "react";
 
 class Quiz extends Component {
   render() {
+    const { quiz, curr_q } = this.props;
     return (
-      <div>
-        <h1>Q: {this.props.quiz[0].question}</h1>
+      <div style={styles.container}>
+        <div style={styles.navBox}>
+          <i className="fa fa-caret-left fa-3x" style={styles.arrowLeft} />
+          <i className="fa fa-caret-right fa-3x" style={styles.arrowRight} />
+        </div>
+        <h2 style={styles.question}>Q: {quiz[curr_q].question}</h2>
       </div>
     );
   }
 }
+
+let styles = {
+  container: {
+    margin: "auto",
+    width: 500,
+    border: "solid",
+    borderWidth: 0.5,
+    borderColor: "black",
+    borderRadius: 5
+  },
+  navBox: {
+    display: "flex",
+    justifyContent: "space-between"
+  },
+  arrowLeft: {
+    alignSelf: "flex-start",
+    margin: 4
+  },
+  arrowRight: {
+    alignSelf: "flex-end",
+    margin: 4
+  },
+  question: {
+    margin: 4
+  }
+};
 
 export default Quiz;

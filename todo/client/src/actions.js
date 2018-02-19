c44d3e5363ccf3e523de850bca8f004467cfbf01
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-02-19 10:37:11 +0900

    action creators done

---
+++ todo/client/src/actions.js
@@ -0,0 +1,26 @@
+import v4 from "uuid";
+import api from "./api";
+
+export const RECEIVE_TODOS = "RECEIVE_TODOS";
+export const ADD_NEW_TODO = "ADD_NEW_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+
+const receiveTodos = () => async dispatch => {
+  const todos = await api.receiveTodos();
+  dispatch({ type: RECEIVE_TODOS, todos });
+};
+
+const addNewTodo = task => async dispatch => {
+  const newTodo = {
+    task,
+    _id: v4(),
+    done: false
+  };
+  await api.addNewTodo(newTodo);
+  dispatch({ type: ADD_NEW_TODO, todo: newTodo });
+};
+
+const toggleTodo = _id => async dispatch => {
+  await api.toggleTodo(_id);
+  dispatch({ type: TOGGLE_TODO, _id });
+};
--- todo/client/src/api.js
+++ todo/client/src/api.js
@@ -1,13 +1,19 @@
-export async function receiveTodos(cb) {
+async function receiveTodos() {
   const response = await fetch("/todo", { method: "get" });
-  const todos = await response.json();
-  cb(todos);
+  return await response.json();
 }
 
-export async function addTodo(new_todo) {
+async function addTodo(new_todo) {
   await fetch("/add", { method: "post", body: new_todo });
 }
 
-export async function toggleTodo(todo) {
+async function toggleTodo(todo) {
   await fetch("/toggle", { method: "post", body: todo });
 }
+
+const api = {
+  receiveTodos,
+  addTodo,
+  toggleTodo
+};
+export default api;

e600cade640208e1b053ba6ed3d6e2e6ac066095
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-02-19 10:11:07 +0900

    server api implemented

--- todo/server.js
+++ todo/server.js
@@ -18,6 +18,29 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.get("/todo", async (req, res) => {
+  const todos = await Todo.Find();
+  res.json(todos);
+});
+
+app.post("/add", async (req, res) => {
+  const newTodo = new Todo(req.body);
+  await newTodo.save();
+  res.json("Todo Saved!");
+});
+
+app.post("/toogle", async (req, res) => {
+  const todo = await Counter.findOne({ _id: req.body._id });
+  todo.done = !todo.done;
+  await todo.save();
+  res.json("toggled!");
+});
+
+app.use((err, request, response, next) => {
+  console.log(err);
+  response.status(500).send("Something broke!");
+});
+
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
   mongoose.connect(mongoDB);

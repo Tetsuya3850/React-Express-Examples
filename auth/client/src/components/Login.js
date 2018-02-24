9cf818cf0a394129697bac56e285b2cca3fa02c5
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-02-24 11:43:03 +0900

    outlined login and register ui

---
+++ auth/client/src/components/Login.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import api from "./api";
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: {}
+    };
+  }
+
+  handleFormSubmit = async e => {
+    e.preventDefault();
+    const payLoad = {
+      email: this.email.value,
+      password: this.password.value
+    };
+    const status = await api.login(payLoad);
+    this.handleStatus(status);
+  };
+
+  handleStatus = status => {
+    if (status !== "Response Added!") {
+      let err = { errors: {} };
+      Object.keys(status.errors).map(key => {
+        err.errors[key] = status.errors[key].message;
+      });
+      this.setState(err);
+    } else {
+      this.clearForm();
+    }
+  };
+
+  clearForm = () => {
+    this.email.value = "";
+    this.password.value = "";
+  };
+
+  render() {
+    return (
+      <div style={{ margin: "auto", width: 400 }}>
+        <form onSubmit={this.handleFormSubmit}>
+          <h2 style={{ textAlign: "center" }}>Login</h2>
+          <hr />
+
+          <label>Email Address</label>
+          <input
+            type="email"
+            style={{ margin: 10 }}
+            ref={node => {
+              this.email = node;
+            }}
+            required
+            maxLength="50"
+            autoFocus
+          />
+          <span style={{ color: "red" }}>*</span>
+          <span style={{ color: "red", marginLeft: 8 }}>
+            {this.state.errors.email}
+          </span>
+          <br />
+
+          <label>Password</label>
+          <input
+            type="password"
+            style={{ margin: 10 }}
+            ref={node => {
+              this.password = node;
+            }}
+            required
+            maxLength="50"
+          />
+          <span style={{ color: "red" }}>*</span>
+          <span style={{ color: "red", marginLeft: 8 }}>
+            {this.state.errors.password}
+          </span>
+          <br />
+
+          <input
+            type="submit"
+            value="Submit!"
+            style={{ display: "block", margin: "auto" }}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ auth/client/src/components/Register.js
@@ -0,0 +1,129 @@
+import React, { Component } from "react";
+import api from "./api";
+
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: {}
+    };
+  }
+
+  handleFormSubmit = async e => {
+    e.preventDefault();
+    const payLoad = {
+      name: this.name.value,
+      email: this.email.value,
+      password: this.password.value
+    };
+    const status = await api.register(payLoad);
+    this.handleStatus(status);
+  };
+
+  handleStatus = status => {
+    if (status !== "Response Added!") {
+      let err = { errors: {} };
+      if (status.code === 11000) {
+        err.errors.email = "Duplicate email!";
+      } else {
+        Object.keys(status.errors).map(key => {
+          err.errors[key] = status.errors[key].message;
+        });
+      }
+      this.setState(err);
+    } else {
+      this.clearForm();
+    }
+  };
+
+  clearForm = () => {
+    this.name.value = "";
+    this.email.value = "";
+    this.password.value = "";
+    this.password2.value = "";
+  };
+
+  render() {
+    return (
+      <div style={{ margin: "auto", width: 400 }}>
+        <form onSubmit={this.handleFormSubmit}>
+          <h2 style={{ textAlign: "center" }}>Register</h2>
+          <hr />
+
+          <label>User Name</label>
+          <input
+            type="text"
+            style={{ margin: 10 }}
+            ref={node => {
+              this.name = node;
+            }}
+            required
+            maxLength="50"
+            autoFocus
+          />
+          <span style={{ color: "red" }}>*</span>
+          <span style={{ color: "red", marginLeft: 8 }}>
+            {this.state.errors.name}
+          </span>
+          <br />
+
+          <label>Email Address</label>
+          <input
+            type="email"
+            style={{ margin: 10 }}
+            ref={node => {
+              this.email = node;
+            }}
+            required
+            maxLength="50"
+          />
+          <span style={{ color: "red" }}>*</span>
+          <span style={{ color: "red", marginLeft: 8 }}>
+            {this.state.errors.email}
+          </span>
+          <br />
+
+          <label>Password</label>
+          <input
+            type="password"
+            style={{ margin: 10 }}
+            ref={node => {
+              this.password = node;
+            }}
+            required
+            maxLength="50"
+          />
+          <span style={{ color: "red" }}>*</span>
+          <span style={{ color: "red", marginLeft: 8 }}>
+            {this.state.errors.password}
+          </span>
+          <br />
+
+          <label>Confirm Password</label>
+          <input
+            type="password"
+            style={{ margin: 10 }}
+            ref={node => {
+              this.password2 = node;
+            }}
+            required
+            maxLength="50"
+          />
+          <span style={{ color: "red" }}>*</span>
+          <span style={{ color: "red", marginLeft: 8 }}>
+            {this.state.errors.password2}
+          </span>
+          <br />
+
+          <input
+            type="submit"
+            value="Submit!"
+            style={{ display: "block", margin: "auto" }}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Register;

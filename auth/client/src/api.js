3747ad33cbd8a7dadc0512045d23252641636169
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-02-24 11:30:34 +0900

    oulined the api calls

---
+++ auth/client/src/api.js
@@ -0,0 +1,35 @@
+async function register(userInfo) {
+  const response = await fetch("/register", {
+    method: "post",
+    body: JSON.stringify(userInfo),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    }
+  });
+  return await response.json();
+}
+
+async function login(userInfo) {
+  const response = await fetch("/login", {
+    method: "post",
+    body: JSON.stringify(userInfo),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    }
+  });
+  return await response.json();
+}
+
+async function getProfile() {
+  const response = await fetch("/profile", { method: "get" });
+  return await response.json();
+}
+
+const api = {
+  register,
+  login,
+  getProfile
+};
+export default api;

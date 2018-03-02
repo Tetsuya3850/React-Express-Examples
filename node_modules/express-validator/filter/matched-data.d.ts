b529703f4dc25cc9dc99e552c30fa463e7bad0e8
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-03-02 09:51:54 +0900

    deleted node modules

--- node_modules/express-validator/filter/matched-data.d.ts
+++
@@ -1,9 +0,0 @@
-import * as express from 'express';
-import { Location } from '../check/location';
-
-export function matchedData(req: express.Request, options?: Partial<MatchedDataOptions>): Record<string, any>;
-
-export interface MatchedDataOptions {
-  onlyValidData: boolean;
-  locations: Location[];
-}

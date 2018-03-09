a1845c62efadd4850400cc89f473e94085fa5bd2
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-03-09 11:16:51 +0900

    todo native frontend initialized

---
+++ todo/native/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+
+import List from "./List";
+import Input from "./Input";
+import Title from "./Title";
+
+export default class App extends Component {
+  state = {
+    todos: ["Click to remove", "Learn React Native", "Write Code", "Ship App"]
+  };
+
+  onAddTodo = text => {
+    const { todos } = this.state;
+
+    this.setState({
+      todos: [text, ...todos]
+    });
+  };
+
+  onRemoveTodo = index => {
+    const { todos } = this.state;
+
+    this.setState({
+      todos: todos.filter((todo, i) => i !== index)
+    });
+  };
+
+  render() {
+    const { todos } = this.state;
+
+    return (
+      <View>
+        <Title>To-Do List</Title>
+        <Input
+          placeholder={"Type a todo, then hit enter!"}
+          onSubmitEditing={this.onAddTodo}
+        />
+        <List list={todos} onPressItem={this.onRemoveTodo} />
+      </View>
+    );
+  }
+}

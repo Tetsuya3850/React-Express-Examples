edeb24341e8654e10bdc87e205e0d4d939d8fd5c
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-03-12 09:01:22 +0900

    initialized auth native app

---
+++ auth/native/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import SignUpForm from "./SignUpForm";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <SignUpForm />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});

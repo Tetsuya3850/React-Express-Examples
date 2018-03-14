7bc03af5cf00194fb680ede0468647675e2fbe00
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-03-14 10:42:58 +0900

    basic authentication working

--- auth/native/App.js
+++ auth/native/App.js
@@ -21,7 +21,7 @@ export default class App extends React.Component {
 
     return (
       <View style={styles.container}>
-        <MainNavigator />
+        <AuthScreen />
       </View>
     );
   }
--- auth/native/screens/AuthScreen.js
+++ auth/native/screens/AuthScreen.js
@@ -1,16 +1,59 @@
 import React, { Component } from "react";
 import { View, Text, Linking } from "react-native";
+import { Constants, WebBrowser, AuthSession } from "expo";
+import queryString from "query-string";
+import { parseToken } from "../utils";
 
 class AuthScreen extends Component {
+  state = {
+    redirectData: null
+  };
+
+  componentDidMount() {
+    Linking.getInitialURL().then(url => {
+      if (url) {
+        this._handleRedirect(url);
+      }
+    });
+  }
+
+  _handleRedirect = url => {
+    let query = url.replace("exp://exp.host/@tetsuya3850/auth", "");
+    const data = queryString.parse(query);
+    const token = data.token;
+
+    this.setState({ redirectData: token });
+  };
+
+  _openWebBrowserAsync = async () => {
+    Linking.openURL(
+      `https://logsignserver.herokuapp.com/auth/facebook/?linkinguri=exp://exp.host/@tetsuya3850/auth`
+    );
+  };
+
+  _maybeRenderRedirectData = () => {
+    if (!this.state.redirectData) {
+      return;
+    }
+
+    return <Text>{this.state.redirectData}</Text>;
+  };
+
   render() {
+    console.log(
+      parseToken(
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YTk0ZDM5OTA3OGQzODRlNmE5YzZmNmEiLCJlbWFpbCI6InRldHN1eWEuY2hpY2Fnb0BnbWFpbC5jb20iLCJuYW1lIjoidGV0c3V5YSIsImV4cCI6MTUyMTU4MjIyOCwiaWF0IjoxNTIwOTc3NDI4fQ.GjjO42xW8A28STpcmeurXBzKHiBPEH_54JLoTRFCnNw#_=_"
+      )
+    );
     return (
       <View>
         <Text
           style={{ color: "blue", textAlign: "center", fontSize: 24 }}
-          onPress={() => Linking.openURL("http://10.0.1.6:5150/auth/facebook")}
+          onPress={this._openWebBrowserAsync}
         >
           Facebook
         </Text>
+        {this._maybeRenderRedirectData()}
       </View>
     );
   }

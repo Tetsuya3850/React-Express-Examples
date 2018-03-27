2bf8065c9631376a97c4059772a25cf5dd4b0670
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-03-27 10:46:23 +0900

    fixed bug

--- switter/client/src/components/SocialAuthRedirect.js
+++ switter/client/src/components/SocialAuthRedirect.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import axios from "axios";
+import { getToken } from "../helper";
 import { socialAuthUser } from "../reducer/users";
 
 class SocialAuthRedirect extends Component {
@@ -7,6 +9,7 @@ class SocialAuthRedirect extends Component {
     const searchParams = new URLSearchParams(window.location.search);
     this.props.dispatch(
       socialAuthUser(searchParams.get("token"), () => {
+        axios.defaults.headers.common["authorization"] = `Bearer ${getToken()}`;
         this.props.history.push("/");
       })
     );

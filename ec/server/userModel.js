b01824b9eb2bee01c26c8939aa42f3788b9c6533
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-04-07 14:55:38 +0900

    db model done

---
+++ ec/server/userModel.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const jwt_secret = process.env.JWT_SECRET;
+
+const validateEmail = email => {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email);
+};
+
+const orderSchema = new mongoose.Schema({
+  cart: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "Product"
+    }
+  ],
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Required!"],
+    validate: [validateEmail, "Invalid address!"],
+    maxlength: [50, "Too Long!"],
+    unique: true,
+    trim: true
+  },
+  name: {
+    type: String,
+    required: [true, "Required!"],
+    maxlength: [50, "Too Long!"],
+    trim: true
+  },
+  cart: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "Product"
+    }
+  ],
+  orders: [orderSchema]
+});
+
+userSchema.methods.generateJwt = function() {
+  let expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      name: this.name,
+      pic: this.pic,
+      exp: parseInt(expiry.getTime() / 1000)
+    },
+    jwt_secret
+  );
+};
+
+mongoose.model("User", userSchema);

fb68e1e637a98e7c9e5c232d30da09b5d7c18985
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2018-04-24 08:37:13 +0900

    fixed link url

--- ec/client/src/components/Auth.js
+++ ec/client/src/components/Auth.js
@@ -6,7 +6,7 @@ class Auth extends Component {
     return (
       <div>
         <a
-          href={`http://localhost:5150/auth/google?linkinguri=${
+          href={`https://ec-server-3850.herokuapp.com/auth/google?linkinguri=${
             window.location.origin
           }/socialauthredirect`}
           style={{ textAlign: "center", display: "block" }}

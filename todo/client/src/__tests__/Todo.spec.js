3fe6fbd540ad54c28dc97bdb6ae00f1bd7ce7774
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2019-03-10 16:33:58 +0900

    removed test from todo client

--- todo/client/src/__tests__/Todo.spec.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Todo from "../components/Todo";
-
-test("Todo renders the task", () => {
-  const container = document.createElement("div");
-  ReactDOM.render(<Todo task={"Run"} />, container);
-  expect(container.textContent).toMatch("Run");
-});

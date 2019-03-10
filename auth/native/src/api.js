98f01604b02021b50515cd17760671d3872596a9
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2019-03-10 09:08:25 +0900

    initialized auth native app

---
+++ auth/native/src/api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { getToken } from "./tokenUtils";
+
+const SERVER_URL = "https://auth-server-3850.herokuapp.com";
+
+const signup = payload => axios.post(`${SERVER_URL}/signup`, payload);
+const signin = payload => axios.post(`${SERVER_URL}/signin`, payload);
+const getUser = () =>
+  axios.get(`${SERVER_URL}/users`, {
+    headers: { authorization: `Bearer ${getToken()}` }
+  });
+
+const api = {
+  signup,
+  signin,
+  getUser
+};
+export default api;
---
+++ auth/native/src/tokenUtils.js
@@ -0,0 +1,28 @@
+export const getToken = () => localStorage.getItem("jwt-token");
+
+export const saveToken = token => localStorage.setItem("jwt-token", token);
+
+export const removeToken = () => localStorage.removeItem("jwt-token");
+
+export const parseToken = () => {
+  try {
+    const token = getToken();
+    if (token) {
+      let payload;
+      payload = token.split(".")[1];
+      payload = window.atob(payload);
+      return JSON.parse(payload);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const isAuthed = () => {
+  const token_info = parseToken();
+  if (token_info && token_info.exp > Math.round(new Date() / 1000)) {
+    return token_info;
+  } else {
+    return false;
+  }
+};

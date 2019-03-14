a4900488a973a64c54c656cbee2ca91f7cc06440
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2019-03-14 13:33:37 +0900

    refactored comment model

--- blog/server/src/comment/commentModel.js
+++ blog/server/src/comment/commentModel.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const commentSchema = new mongoose.Schema({
-  userId: {
+  author: {
     type: Schema.Types.ObjectId,
     ref: "User"
   },
-  articleId: {
+  article: {
     type: Schema.Types.ObjectId,
     ref: "Article"
   },

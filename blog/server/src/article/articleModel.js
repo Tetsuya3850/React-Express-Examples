4c1a922e9c123ce9cf60b086bd82381028ded35a
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2019-03-14 13:32:47 +0900

    changed aritcle model field name

--- blog/server/src/article/articleModel.js
+++ blog/server/src/article/articleModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const articleSchema = new mongoose.Schema({
-  userId: {
+  author: {
     type: Schema.Types.ObjectId,
     ref: "User"
   },

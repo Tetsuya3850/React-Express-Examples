9165259b1785af5a759c21085d3d975ef85843ce
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2019-03-26 12:44:57 +0900

    renamed to twitter and deleted questionnaire

--- questionnaire/client/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Questionnaire from "./Questionnaire";
-
-ReactDOM.render(<Questionnaire />, document.getElementById("root"));
---
+++ twitter/README.md
@@ -0,0 +1,9 @@
+Twitter example written by React.js, React Native, and Node.js
+
+## Features
+
+- Copy Twitter!
+
+## Demo
+
+(https://switter-client-3850.herokuapp.com/)

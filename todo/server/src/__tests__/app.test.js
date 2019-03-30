5192f814e2e1c8c7ac1ad95a4e64d3f901a64671
Tetsuya Hasegawa <tetsuya.chicago@gmail.com>
2019-03-31 07:56:09 +0900

    todo app refactor

--- todo/server/src/__tests__/app.test.js
+++ todo/server/src/__tests__/app.test.js
@@ -1,21 +1,16 @@
-let baseURL, api, server;
 const axios = require("axios");
-const _ = require("lodash");
 const mongoose = require("mongoose");
-const start = require("../app");
+const _ = require("lodash");
 
-beforeAll(() => {
-  server = start();
-  baseURL = `http://localhost:${server.address().port}`;
-  api = axios.create({ baseURL });
-});
+const server = require("../../app");
+const baseURL = `http://localhost:${server.address().port}`;
+const api = axios.create({ baseURL });
+const Todo = mongoose.model("Todo");
 
 afterAll(async () => {
   await Todo.deleteMany({});
   server.close();
 });
-
-const Todo = mongoose.model("Todo");
 
 const getData = res => res.data;
 const getError = error => error.response;
@@ -31,51 +26,57 @@ const anotherTestTodo = {
   text: "Run"
 };
 
-test("postTodo: text required", async () => {
-  const error = await api
-    .post("/todos", _.omit(testTodo, "text"))
-    .catch(getError);
-  expect(error).toMatchObject({
-    status: 400
+describe("postTodo:", () => {
+  it("text required", async () => {
+    const error = await api
+      .post("/todos", _.omit(testTodo, "text"))
+      .catch(getError);
+    expect(error).toMatchObject({
+      status: 400
+    });
+  });
+
+  it("Success", async () => {
+    const todo = await api.post("/todos", testTodo).then(getData);
+    expect(todo).toMatchObject({
+      ...testTodo,
+      _id: expect.any(String)
+    });
   });
 });
 
-test("postTodo: Success", async () => {
-  const todo = await api.post("/todos", testTodo).then(getData);
-  expect(todo).toMatchObject({
-    ...testTodo,
-    _id: expect.any(String)
+describe("getTodo:", () => {
+  it("Success", async () => {
+    await api.post("/todos", anotherTestTodo);
+    const todos = await api.get("/todos").then(getData);
+    expect(todos[0]).toMatchObject({
+      ...anotherTestTodo,
+      _id: expect.any(String)
+    });
+    expect(todos[1]).toMatchObject({
+      ...testTodo,
+      _id: expect.any(String)
+    });
   });
 });
 
-test("getTodo: Success", async () => {
-  await api.post("/todos", anotherTestTodo);
-  const todos = await api.get("/todos").then(getData);
-  expect(todos[0]).toMatchObject({
-    ...anotherTestTodo,
-    _id: expect.any(String)
+describe("deleteTodo:", () => {
+  it("Error in deleting non existing todo", async () => {
+    const todoId = new mongoose.Types.ObjectId();
+    const error = await api.delete(`/todos/${todoId}`).catch(getError);
+    expect(error).toMatchObject({
+      status: 404
+    });
+    const todos = await api.get("/todos").then(getData);
+    expect(todos).toHaveLength(2);
   });
-  expect(todos[1]).toMatchObject({
-    ...testTodo,
-    _id: expect.any(String)
-  });
-});
 
-test("deleteTodo: Error in deleting non existing todo", async () => {
-  const todoId = new mongoose.Types.ObjectId();
-  const error = await api.delete(`/todos/${todoId}`).catch(getError);
-  expect(error).toMatchObject({
-    status: 404
+  it("Success", async () => {
+    const todos = await api.get("/todos").then(getData);
+    const deleteTodo = todos[0];
+    const status = await api.delete(`/todos/${deleteTodo._id}`).then(getStatus);
+    expect(status).toBe(200);
+    const less_todos = await api.get("/todos").then(getData);
+    expect(less_todos).toEqual(expect.not.arrayContaining([deleteTodo]));
   });
-  const todos = await api.get("/todos").then(getData);
-  expect(todos).toHaveLength(2);
-});
-
-test("deleteTodo: Success", async () => {
-  const todos = await api.get("/todos").then(getData);
-  const deleteTodo = todos[0];
-  const status = await api.delete(`/todos/${deleteTodo._id}`).then(getStatus);
-  expect(status).toBe(200);
-  const less_todos = await api.get("/todos").then(getData);
-  expect(less_todos).toEqual(expect.not.arrayContaining([deleteTodo]));
 });
